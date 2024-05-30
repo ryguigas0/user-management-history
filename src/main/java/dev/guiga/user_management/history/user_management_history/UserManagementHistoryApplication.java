e3315c51e0403dfc293206b0912e78ef6105aa6b
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2024-05-30 09:36:24 -0300

    Created project

---
+++ src/main/java/dev/guiga/user_management/history/user_management_history/UserManagementHistoryApplication.java
@@ -0,0 +1,13 @@
+package dev.guiga.user_management.history.user_management_history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/guiga/user_management/history/user_management_history/UserManagementHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guiga.user_management.history.user_management_history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
